071ee61376212e93c9256d8a0560019c65711c16
RohanPhaff <70520082+RohanPhaff@users.noreply.github.com>
2021-12-22 19:58:31 +0100

    Aangepaste link A2

--- resources/views/niveau-results/a2result.blade.php
+++ resources/views/niveau-results/a2result.blade.php
@@ -25,9 +25,9 @@
 @if ($score >= 25)
     <p class="result" style="margin-top: 5%;">Je hebt meer dan 25 vragen goed beantwoord en daarmee het niveau A2 al aardig onder de knie. Om
         verder te gaan met het leren van Spaans zijn er voor jou twee opties:</p>
-    <p class="result">A. Je kunt deelnemen aan het praktijkjaar A1/A2. Tijdens dit jaar vol praktische lessen slijt
+    <p class="result">A. Je kunt deelnemen aan de conversatie groep. Tijdens dit jaar vol conversaties slijt
         de kennis van het beginnersniveau in en leer je het geleerde vlotter toe te passen. Meer
-        informatie over dit praktijk jaar vind je hier: <a href="https://la-maravilla.nl/conversatie">Praktijkjaar A1/A2</a></p>
+        informatie over dit conversatie jaar vind je hier: <a href="https://la-maravilla.nl/conversatie">Conversatie Jaar</a></p>
     <p class="result">B. Probeer de niveautest van niveau B1 om te zien welke cursus bij jou aansluit. Om verder
         te gaan met de volgende niveautest kan je hier klikken: <a href="b1/create">Niveautest B1</a></p>
 @endif
